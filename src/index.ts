5979a35f2d1a37483cd5737d2abbafbcecb9eafe
shra1 <shravan@attivonetworks.com>
2019-12-15 19:36:26 +0530

    Added Validation type

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { StrengthScore } from './StrengthScore';
+type ValidationType = 'minLength' | 'maxLength' | 'lowercase' | 'uppercase' | 'number' | 'dictionary' | 'common';
 
 // "use strict";
-export default module.exports = (inputString: string = "Hello Friend", props: string[] = []) => {
+export default module.exports = (inputString: string = "Hello Friend", props: ValidationType[] = []) => {
   console.log(inputString);
   let params = ['uppercase', 'lowercase', 'number', ...props];
   const score = StrengthScore(inputString, params);
